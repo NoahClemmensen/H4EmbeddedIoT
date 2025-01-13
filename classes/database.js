09988d9ca024e294d3d3335f830eb3a5012da16e
NoahClemmensen <noah0940@edu.sde.dk>
2025-01-13 12:46:11 +0100

    Db users

--- classes/database.js
+++ classes/database.js
@@ -1,15 +1,25 @@
 const mysql = require('mysql2/promise');
 
-let config = {
+let procedureConf = {
     socketPath: process.env.DB_SOCKET,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    user: process.env.DB_PROCEDURE_USER,
+    password: process.env.DB_PROCEDURE_PASSWORD,
     database: process.env.DB_NAME,
 }
 
-let connection = mysql.createConnection(config);
+let selectConf = {
+    socketPath: process.env.DB_SOCKET,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_SELECT_USER,
+    password: process.env.DB_SELECT_PASSWORD,
+    database: process.env.DB_NAME,
+}
+
+let procedureConn = mysql.createConnection(procedureConf);
+let selectConn = mysql.createConnection(selectConf);
 
 class Database {
     static async getDevices() {
@@ -21,35 +31,35 @@ class Database {
     }
 
     static async getSettings() {
-        return this.query('call get_settings()');
+        return this.queryProcedure('get_settings()');
     }
 
     static async saveSettings(settings) {
-        return this.query('call change_settings(?,?,?,?,?,?,?,?,?)', [settings.max_temp, settings.min_temp, settings.max_fugt, settings.min_fugt, settings.temp_interval, settings.fugt_interval, settings.start_time, settings.end_time, settings.password]);
+        return this.queryProcedure('change_settings(?,?,?,?,?,?,?,?,?)', [settings.max_temp, settings.min_temp, settings.max_fugt, settings.min_fugt, settings.temp_interval, settings.fugt_interval, settings.start_time, settings.end_time, settings.password]);
     }
 
     static async logTemp(temperature, time, deviceSN) {
-        return this.query('call log_temp(?,?,?)', [temperature, new Date(time), deviceSN]);
+        return this.queryProcedure('log_temp(?,?,?)', [temperature, new Date(time), deviceSN]);
     }
 
     static async logHumidity(humidity, time, deviceSN) {
-        return this.query('call log_humidity(?,?,?)', [humidity, new Date(time), deviceSN]);
+        return this.queryProcedure('log_humidity(?,?,?)', [humidity, new Date(time), deviceSN]);
     }
 
     static async getClimateData(deviceSN) {
-        return this.query('call get_climate_info(?)', [deviceSN]);
+        return this.queryProcedure('get_climate_info(?)', [deviceSN]);
     }
 
     static async editDevice(sn, location, name, desc) {
-        return this.query('call edit_device(?,?,?,?)', [sn, location, name, desc]);
+        return this.queryProcedure('edit_device(?,?,?,?)', [sn, location, name, desc]);
     }
 
     static async deleteDevice(sn) {
-        return this.query('call delete_device(?)', [sn]);
+        return this.queryProcedure('delete_device(?)', [sn]);
     }
 
     static async createDevice(sn, location, name, desc) {
-        return this.query('call create_device(?,?,?,?)', [sn, location, name, desc]);
+        return this.queryProcedure('create_device(?,?,?,?)', [sn, location, name, desc]);
     }
 
     static async getPassword() {
@@ -58,11 +68,20 @@ class Database {
     }
 
     static async query(sql, args) {
-        if (connection instanceof Promise) {
-            connection = await connection;
+        if (selectConn instanceof Promise) {
+            selectConn = await selectConn;
         }
 
-        const [results, fields] = await connection.query(sql, args);
+        const [results, fields] = await selectConn.query(sql, args);
+        return results;
+    }
+
+    static async queryProcedure(sql, args) {
+        if (procedureConn instanceof Promise) {
+            procedureConn = await procedureConn;
+        }
+
+        const [results, fields] = await procedureConn.query('call ' + sql, args);
         return results;
     }
 }
